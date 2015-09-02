e35d2e1456d6868bb21fc403b366b9b4da750314
Ben Bader <ben@getkeepsafe.com>
2015-09-02 15:38:29 -0700

    Consistently apply license header and copyright attribution

--- src/main/java/com/android/dexdeps/FieldRef.java
+++ src/main/java/com/android/dexdeps/FieldRef.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2015 KeepSafe Software
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
