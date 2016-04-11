79d6ee35f71015173e24ddd6401f3b4131b56ed1
Ken Yee <ken.yee@raizlabs.com>
2016-04-11 13:27:49 -0400

    test workaround for travis not finding AAR test file in right place

--- src/test/groovy/com/getkeepsafe/dexcount/DexFileTest.groovy
+++ src/test/groovy/com/getkeepsafe/dexcount/DexFileTest.groovy
@@ -11,6 +11,25 @@ class DexFileTest extends Specification {
         def aarFile = new File(currentDir + File.separatorChar + "src"
                 + File.separatorChar + "test" + File.separatorChar + "resources"
                 + File.separatorChar + "android-beacon-library-2.7.aar")
+        def url = getClass().getResource("./")
+        def files = new File(url.toURI()).listFiles()
+        for (file in files) {
+            println(file.absolutePath)
+        }
+        if (!aarFile.exists()) {
+            aarFile = File.createTempFile("test", ".aar")
+            aarFile.deleteOnExit()
+            def buf = new byte[4096]
+            getClass().getResourceAsStream('android-beacon-library-2.7.aar').withStream { input ->
+                aarFile.withOutputStream { output ->
+                    def read
+                    while ((read = input.read(buf)) != -1) {
+                        output.write(buf, 0, read)
+                    }
+                    output.flush()
+                }
+            }
+        }
         def dexFiles = DexFile.extractDexData(aarFile)
 
         then:
