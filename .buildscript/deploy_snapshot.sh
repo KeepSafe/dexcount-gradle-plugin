41ed6391e8df267587843e0d13323c50f9092469
Ben Bader <ben@bendb.com>
2020-07-22 14:25:17 -0700

    Remove those 'exit 1' statements from deploy_snapshot.sh

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -13,13 +13,10 @@ set -e
 
 if [ "$GITHUB_REPOSITORY" != "$SLUG" ]; then
   echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-  exit 1
 elif [ "$GITHUB_EVENT_NAME" != "$EVENT" ]; then
   echo "Skipping snapshot deployment: was pull request."
-  exit 1
 elif [ "$GITHUB_REF" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
-  exit 1
 else
   echo "Deploying snapshot..."
   ./gradlew clean publish
