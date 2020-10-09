89c2261bd55c1b0feea0257f1b78e7ff7faf8fbe
Ben Bader <ben.bader@instacart.com>
2020-10-08 23:18:04 -0600

    Apply terrible hack

--- src/test/groovy/com/getkeepsafe/dexcount/DexCountExtensionSpec.groovy
+++ src/test/groovy/com/getkeepsafe/dexcount/DexCountExtensionSpec.groovy
@@ -22,6 +22,8 @@ import com.android.build.api.variant.BuiltArtifactsLoader
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
+import org.gradle.api.internal.project.ProjectInternal
+import org.gradle.initialization.GradlePropertiesController
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
@@ -35,6 +37,7 @@ final class DexCountExtensionSpec extends Specification {
 
     def "setup"() {
         project = ProjectBuilder.builder().build()
+        (project as ProjectInternal).services.get(GradlePropertiesController.class).loadGradlePropertiesFrom(project.rootDir)
 
         def manifestFile = project.file('src/main/AndroidManifest.xml')
         manifestFile.parentFile.mkdirs()
