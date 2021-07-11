a11e0c39de2266d7724f23376f006b79ce58266d
Ben Bader <benjamin-bader@users.noreply.github.com>
2021-07-10 21:27:17 -0600

    Re-enable support for AGP 7.1.0-alpha03 (#378)

--- src/integrationTest/groovy/com/getkeepsafe/dexcount/IntegrationSpec.groovy
+++ src/integrationTest/groovy/com/getkeepsafe/dexcount/IntegrationSpec.groovy
@@ -30,7 +30,7 @@ class IntegrationSpec extends Specification {
 
         where:
         agpVersion      | gradleVersion || numMethods | numClasses | numFields
-        //"7.1.0-alpha02" | "7.0.2"       || 7421       | 926        | 2676
+        "7.1.0-alpha03" | "7.0.2"       || 7355       | 926        | 2590
         "7.0.0-beta05"  | "7.0.2"       || 7355       | 926        | 2590
         "4.2.0"         | "6.8.1"       || 7422       | 926        | 2677
         "4.1.0"         | "6.7.1"       || 7356       | 926        | 2597
@@ -60,7 +60,7 @@ class IntegrationSpec extends Specification {
 
         where:
         agpVersion      | gradleVersion || numMethods | numClasses | numFields
-        //"7.1.0-alpha02" | "7.0.2"       || 7          | 5          | 3
+        "7.1.0-alpha03" | "7.0.2"       || 7          | 5          | 3
         "7.0.0-beta05"  | "7.0.2"       || 7          | 5          | 3
         "4.2.0"         | "6.8.1"       || 7          | 6          | 3
         "4.1.0"         | "6.7.1"       || 7          | 6          | 3
@@ -90,7 +90,7 @@ class IntegrationSpec extends Specification {
 
         where:
         agpVersion         | gradleVersion || numMethods | numClasses | numFields
-        //"7.1.0-alpha02"    | "7.0.2"       || 4266       | 723        | 1268
+        "7.1.0-alpha03"    | "7.0.2"       || 4266       | 723        | 1268
         "7.0.0-beta05"     | "7.0.2"       || 4266       | 723        | 1268
         "4.2.0"            | "6.8.1"       || 4266       | 723        | 1268
         "4.1.0"            | "6.7.1"       || 4266       | 723        | 1268
@@ -120,7 +120,7 @@ class IntegrationSpec extends Specification {
 
         where:
         agpVersion      | gradleVersion || numMethods | numClasses | numFields
-        //"7.1.0-alpha02" | "7.0.2"       || 7421       | 926        | 2676
+        "7.1.0-alpha03" | "7.0.2"       || 7355       | 926        | 2590
         "7.0.0-beta05"  | "7.0.2"       || 7355       | 926        | 2590
         "4.2.0"         | "6.8.1"       || 7422       | 926        | 2677
         "4.1.0"         | "6.7.1"       || 7356       | 926        | 2597
