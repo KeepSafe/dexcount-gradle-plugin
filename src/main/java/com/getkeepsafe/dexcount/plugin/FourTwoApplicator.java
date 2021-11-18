cae880e970f8061209e4b76219906c43b22eb740
Ben Bader <benjamin-bader@users.noreply.github.com>
2021-11-18 09:50:05 -0700

    Support AGP 7.2.0-alpha05 (#419)

--- src/main/java/com/getkeepsafe/dexcount/plugin/FourTwoApplicator.java
+++ src/main/java/com/getkeepsafe/dexcount/plugin/FourTwoApplicator.java
@@ -17,17 +17,25 @@ package com.getkeepsafe.dexcount.plugin;
 
 import com.android.build.api.artifact.Artifacts;
 import com.android.build.api.dsl.CommonExtension;
+import com.android.build.api.variant.ApplicationVariant;
+import com.android.build.api.variant.LibraryVariant;
+import com.android.build.api.variant.TestVariant;
 import com.android.build.gradle.AppPlugin;
 import com.android.build.gradle.LibraryPlugin;
 import com.android.build.gradle.TestPlugin;
 import com.android.repository.Revision;
+import com.getkeepsafe.dexcount.DexCountException;
 import com.getkeepsafe.dexcount.DexCountExtension;
 import com.getkeepsafe.dexcount.treegen.LibraryPackageTreeTask;
 import org.apache.commons.lang3.StringUtils;
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.file.RegularFile;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.TaskProvider;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 class FourTwoApplicator extends FourOneApplicator {
     static class Factory implements TaskApplicator.Factory {
@@ -42,6 +50,20 @@ class FourTwoApplicator extends FourOneApplicator {
         }
     }
 
+    private static final String ANDROID_COMPONENTS_EXTENSION =
+        "com.android.build.api.extension.AndroidComponentsExtension";
+
+    private static final String APPLICATION_ANDROID_COMPONENTS_EXTENSION =
+        "com.android.build.api.extension.ApplicationAndroidComponentsExtension";
+
+    private static final String LIBRARY_ANDROID_COMPONENTS_EXTENSION =
+        "com.android.build.api.extension.LibraryAndroidComponentsExtension";
+
+    private static final String TEST_ANDROID_COMPONENTS_EXTENSION =
+        "com.android.build.api.extension.TestAndroidComponentsExtension";
+
+    private static final String VARIANT_SELECTOR = "com.android.build.api.extension.VariantSelector";
+
     FourTwoApplicator(Project project, DexCountExtension ext) {
         super(project, ext);
     }
@@ -53,32 +75,83 @@ class FourTwoApplicator extends FourOneApplicator {
             return;
         }
 
-        getProject().getPlugins().withType(AppPlugin.class).configureEach(plugin -> {
-            com.android.build.api.extension.ApplicationAndroidComponentsExtension androidComponents =
-                getProject().getExtensions().getByType(com.android.build.api.extension.ApplicationAndroidComponentsExtension.class);
+        applyReflectively();
+    }
 
-            androidComponents.onVariants(androidComponents.selector().all(), variant -> {
-                registerApkTask(variant.getName(), variant.getArtifacts());
-                registerBundleTask(variant.getName(), variant.getArtifacts());
-            });
+    private void applyReflectively() {
+        Class<?> clsAndroidComponentsExtension;
+        Method fnOnVariants;
+        Method fnSelector;
+
+        Class<?> clsAppComponentsExtension;
+        Class<?> clsLibraryComponentsExtension;
+        Class<?> clsTestComponentsExtension;
+
+        Class<?> clsVariantSelector;
+        Method fnVariantSelectorAll;
+
+        try {
+            clsVariantSelector = Class.forName(VARIANT_SELECTOR);
+            fnVariantSelectorAll = clsVariantSelector.getDeclaredMethod("all");
+
+            clsAndroidComponentsExtension = Class.forName(ANDROID_COMPONENTS_EXTENSION);
+            fnOnVariants = clsAndroidComponentsExtension.getDeclaredMethod("onVariants", clsVariantSelector, Action.class);
+            fnSelector = clsAndroidComponentsExtension.getDeclaredMethod("selector");
+
+            clsAppComponentsExtension = Class.forName(APPLICATION_ANDROID_COMPONENTS_EXTENSION);
+            clsLibraryComponentsExtension = Class.forName(LIBRARY_ANDROID_COMPONENTS_EXTENSION);
+            clsTestComponentsExtension = Class.forName(TEST_ANDROID_COMPONENTS_EXTENSION);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            throw new DexCountException("Failed to load AGP types", e);
+        }
+
+        getProject().getPlugins().withType(AppPlugin.class).configureEach(plugin -> {
+            try {
+                Object androidComponents = getProject().getExtensions().getByType(clsAppComponentsExtension);
+                Object selector = fnSelector.invoke(androidComponents);
+                Object allVariants = fnVariantSelectorAll.invoke(selector);
+
+                Action<ApplicationVariant> action = variant -> {
+                    registerApkTask(variant.getName(), variant.getArtifacts());
+                    registerBundleTask(variant.getName(), variant.getArtifacts());
+                };
+
+                fnOnVariants.invoke(androidComponents, allVariants, action);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new DexCountException("Failed to apply dexcount to application", e);
+            }
         });
 
         getProject().getPlugins().withType(LibraryPlugin.class).configureEach(plugin -> {
-            com.android.build.api.extension.LibraryAndroidComponentsExtension androidComponents =
-                getProject().getExtensions().getByType(com.android.build.api.extension.LibraryAndroidComponentsExtension.class);
+            try {
+                Object libraryComponents = getProject().getExtensions().getByType(clsLibraryComponentsExtension);
+                Object selector = fnSelector.invoke(libraryComponents);
+                Object allVariants = fnVariantSelectorAll.invoke(selector);
 
-            androidComponents.onVariants(androidComponents.selector().all(), variant -> {
-                registerAarTask(variant.getName(), variant.getArtifacts());
-            });
+                Action<LibraryVariant> action = variant -> {
+                    registerAarTask(variant.getName(), variant.getArtifacts());
+                };
+
+                fnOnVariants.invoke(libraryComponents, allVariants, action);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new DexCountException("Failed to apply dexcount to library", e);
+            }
         });
 
         getProject().getPlugins().withType(TestPlugin.class).configureEach(plugin -> {
-            com.android.build.api.extension.TestAndroidComponentsExtension androidComponents =
-                getProject().getExtensions().getByType(com.android.build.api.extension.TestAndroidComponentsExtension.class);
+            try {
+                Object testComponents = getProject().getExtensions().getByType(clsTestComponentsExtension);
+                Object selector = fnSelector.invoke(testComponents);
+                Object allVariants = fnVariantSelectorAll.invoke(selector);
 
-            androidComponents.onVariants(androidComponents.selector().all(), variant -> {
-                registerApkTask(variant.getName(), variant.getArtifacts());
-            });
+                Action<TestVariant> action = variant -> {
+                    registerApkTask(variant.getName(), variant.getArtifacts());
+                };
+
+                fnOnVariants.invoke(testComponents, allVariants, action);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new DexCountException("Failed to apply dexcount to test", e);
+            }
         });
 
         getProject().afterEvaluate(project -> {
